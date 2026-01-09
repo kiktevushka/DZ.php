4ac465ff5db9026ee19a9128ddca38d29dfa8e6c
kiktevushka <flatiron119@yandex.ru>
2026-01-09 19:48:22 +0300

    Update DZ_first.php

--- DZ_first.php
+++ DZ_first.php
@@ -1,22 +1,23 @@
 <?php
 
-$n = readline("Имя: ");
-$s = readline("Фамилия: ");
-$p = readline("Отчество: ");
+
+$name = readline("Имя: ");
+$surName = readline("Фамилия: ");
+$lastName = readline("Отчество: ");
 
 
-$name = mb_convert_case($n, MB_CASE_TITLE, "UTF-8");
-$surname = mb_convert_case($s, MB_CASE_TITLE, "UTF-8");
-$patronymic = mb_convert_case($p, MB_CASE_TITLE, "UTF-8");
+$nameCapital = mb_convert_case($name, MB_CASE_TITLE, "UTF-8");
+$surnameCapital = mb_convert_case($surName, MB_CASE_TITLE, "UTF-8");
+$lastNameCapital = mb_convert_case($lastName, MB_CASE_TITLE, "UTF-8");
 
-$fullName = "$surname $name $patronymic";
-$ni = mb_substr($name, 0, 1, "UTF-8");
-$pi = mb_substr($patronymic, 0, 1, "UTF-8");
-$si = mb_substr($surname, 0, 1, "UTF-8");
+$fullName = "$surnameCapital $nameCapital $lastNameCapital";
+$nameInitial = mb_substr($nameCapital, 0, 1, "UTF-8");
+$lastNameInitial = mb_substr($lastNameCapital, 0, 1, "UTF-8");
+$surNameInitial = mb_substr($surnameCapital, 0, 1, "UTF-8");
 
 echo "\nРезультат:\n";
 echo "Полное имя: $fullName\n";
-echo "Инициалы: $surname $ni.$pi.\n";
-echo "Аббревиатура: $si$ni$pi\n";
+echo "Инициалы: $surnameCapital $nameInitial.$lastNameInitial.\n";
+echo "Аббревиатура: $surNameInitial$nameInitial$lastNameInitial\n";
 
 ?>
